29e0ca1f6af19bbc7f9642077de221e54ca247ea
val44087 <61064107+val44087@users.noreply.github.com>
2020-02-17 20:53:22 +0000

    DoneDay1

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,18 @@
 
 import './styles.css';
-console.log('Ready to Party With Some TypeScript!');
+import { add } from './math';
 
+const num1Element = document.getElementById('num1') as HTMLInputElement;
+const num2Element = document.getElementById('num2') as HTMLInputElement;
+const addButton = document.getElementById('add') as HTMLButtonElement;
+const answerElments = document.getElementById('answer') as HTMLInputElement;
+
+addButton.addEventListener('click', function () {
+    const a = num1Element.valueAsNumber;
+    const b = num2Element.valueAsNumber;
+
+    const sum = add(a, b);
+
+    answerElments.innerText = sum.toString();
+
+});
